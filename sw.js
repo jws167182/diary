e801fa039db2e13b9035bae47edf6ebfacc2b8c8
jws167182 <jws167182@gmail.com>
2026-09-19 00:32:34 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE='diary-pwa-sync-v1';
+
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c=>c.addAll([
+        './manifest.webmanifest',
+        './icon-192.png',
+        './icon-512.png'
+      ]))
+      .catch(()=>{})
+  );
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(
+        keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+      ))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',e=>{
+  if(e.request.mode==='navigate'){
+    e.respondWith(
+      fetch(e.request)
+        .then(r=>{
+          const copy=r.clone();
+          caches.open(CACHE).then(c=>c.put('./index.html',copy));
+          return r;
+        })
+        .catch(()=>caches.match('./index.html'))
+    );
+    return;
+  }
+
+  e.respondWith(
+    fetch(e.request).catch(()=>caches.match(e.request))
+  );
+});
